1acade2bc786bc641c57b934fbb858bc883a2176
NitescuLucian <nitesculuci@gmail.com>
2017-07-04 09:49:29 +0300

    First tool of this project!

--- NliWebSec/sourcecodeleak.cs
+++ NliWebSec/sourcecodeleak.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NliWebSec
@@ -12,6 +13,7 @@ namespace NliWebSec
     {
         static void Main(string[] args)
         {
+            string html = null;
             Console.WriteLine("Please enter the url of your target: ");
             string url = Console.ReadLine();
             // reading the html response from the target
@@ -31,13 +33,17 @@ namespace NliWebSec
                     readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
                 }
 
-                string html = readStream.ReadToEnd();
-                
+                html = readStream.ReadToEnd();
+
                 response.Close();
                 readStream.Close();
             }
             //searching for urls / paths in the html response of the target
-
+            foreach (Match item in Regex.Matches(html, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
