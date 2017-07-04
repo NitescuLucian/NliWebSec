5c16c6821bc2fc766a6caf04e34bade0202d52bf
NitescuLucian <nitesculuci@gmail.com>
2017-07-04 10:58:07 +0300

    I started learning C# on 03/07/2017 ... now ....

--- NliWebSec/sourcecodeleak.cs
+++ NliWebSec/sourcecodeleak.cs
@@ -18,13 +18,17 @@ namespace NliWebSec
             string html = null;
             Console.WriteLine("Please enter the root url of your target: ");
             string url = Console.ReadLine();
-            /*Console.WriteLine("Please enter the level: ");
-            int lvl = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < lvl; i++)
+            Console.WriteLine("Please enter the level: ");
+            int lvl = 0;
+            string line = Console.ReadLine();
+            try
             {
-
-            }*/
+                lvl = Int32.Parse(line);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\"{0}\" is not an integer! Performing one scan on one level!", line);
+            }
             // reading the html response from the target
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -56,11 +60,55 @@ namespace NliWebSec
                     mytargets.Add(item.Value);
                 }
             }
+            //this is how I set the depth of my scan
             mytargets.ToArray();
+            if (lvl > 1)
+            {
+                for (int i = 0; i < lvl; i++)
+                {
+                    foreach (string item in mytargets)
+                    {
+                        url = item;
+                        HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url);
+                        HttpWebResponse response1 = (HttpWebResponse)request1.GetResponse();
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            Stream receiveStream = response1.GetResponseStream();
+                            StreamReader readStream = null;
+
+                            if (response.CharacterSet == null)
+                            {
+                                readStream = new StreamReader(receiveStream);
+                            }
+                            else
+                            {
+                                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                            }
+
+                            html = readStream.ReadToEnd();
+
+                            response.Close();
+                            readStream.Close();
+                        }
+                        //searching for urls / paths in the html response of the target
+                        foreach (Match item1 in Regex.Matches(html, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
+                        {
+                            string draft = item1.Value;
+                            //the role of this if is to filter false positives and 3rd parties resources from crawl
+                            if (draft.StartsWith(url))
+                            {
+                                mytargets.Add(item1.Value);
+                            }
+                        }
+                    }
+                }
+            }
+           
             for (int i = 0; i < mytargets.Count(); i++)
             {
                 Console.WriteLine(mytargets[i]);
             }
+            Console.WriteLine("At this point we found out: " + mytargets.Count() + " urls");
             Console.WriteLine();
             Console.ReadLine();
         }
