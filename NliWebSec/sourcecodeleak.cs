c5bb705972696a9c00c58b72d0b5cc57d02f1a6d
NitescuLucian <nitesculuci@gmail.com>
2017-07-04 13:10:18 +0300

    Postponing problems

--- NliWebSec/sourcecodeleak.cs
+++ NliWebSec/sourcecodeleak.cs
@@ -16,7 +16,9 @@ namespace NliWebSec
         {
             List<string> mytargets = new List<string>();
             HttpWebResponse response = null;
+            HttpWebResponse response1 = null;
             string html = null;
+            string html1 = null;
             Console.WriteLine("Please enter the root url of your target: ");
             string url = Console.ReadLine();
             Console.WriteLine("Please enter the level: ");
@@ -72,60 +74,72 @@ namespace NliWebSec
                     mytargets.Add(item.Value);
                 }
             }
-            //this is how I set the depth of my scan
-            mytargets.ToArray();
+/*
             if (lvl > 1)
             {
                 for (int i = 0; i < lvl; i++)
                 {
-                    foreach (string item1 in mytargets)
+                    for (int j = 0; j < mytargets.Count; j++)
                     {
-
-                        Stream receiveStream = null;
-                        HttpWebResponse response1 = null;
-                        url = item1;
-                        HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url);
-                        try
+                        string url1 = mytargets[j];
+                        if (url1 != null)
                         {
-                            response1 = (HttpWebResponse)request1.GetResponse();
-                        }
-                        catch (System.Net.WebException ex)
-                        {
-                            Debug.WriteLine("Exception Message: " + ex.Message);
-                        }
-
-                        if (response1 != null)
-                        {
-                            receiveStream = response1.GetResponseStream();
-                            StreamReader readStream = null;
-                            if (response.CharacterSet == null)
+                            // reading the html response from the target
+                            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url1);
+                            if (request1 != null)
                             {
-                                readStream = new StreamReader(receiveStream);
-                            }
-                            else
-                            {
-                                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-                            }
+                                try
+                                {
+                                    response1 = (HttpWebResponse)request1.GetResponse();
+                                }
+                                catch (System.Net.WebException ex)
+                                {
+                                    Debug.WriteLine("Exception Message: " + ex.Message);
+                                }
+                                if (response1 != null)
+                                {
+                                    if (response1.StatusCode == HttpStatusCode.OK)
+                                    {
+                                        Stream receiveStream1 = response1.GetResponseStream();
+                                        StreamReader readStream1 = null;
 
-                            html = readStream.ReadToEnd();
+                                        if (response1.CharacterSet == null)
+                                        {
+                                            readStream1 = new StreamReader(receiveStream1);
+                                        }
+                                        else
+                                        {
+                                            readStream1 = new StreamReader(receiveStream1, Encoding.GetEncoding(response1.CharacterSet));
+                                        }
 
-                            response1.Close();
-                            readStream.Close();
+                                        html1 = readStream1.ReadToEnd();
+
+                                        response1.Close();
+                                        readStream1.Close();
+                                    }
+                                }
+                            }
                         }
-                    }
-                    //searching for urls / paths in the html response of the target
-                    foreach (Match item in Regex.Matches(html, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
-                    {
-                        string draft = item.Value;
-                        //the role of this if is to filter false positives and 3rd parties resources from crawl
-                        if (draft.StartsWith(url))
+                        //searching for urls / paths in the html response of the target
+                        if (html1 != null)
                         {
-                            mytargets.Add(item.Value);
+                            foreach (Match item1 in Regex.Matches(html1, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
+                            {
+                                string draft1 = item1.Value;
+                                //the role of this if is to filter false positives and 3rd parties resources from crawl
+                                if (draft1.StartsWith(url))
+                                {
+                                    mytargets.Add(item1.Value);
+                                }
+                            }
                         }
+
                     }
+
                 }
             }
-
+*/
+            mytargets.ToArray();
 
             for (int i = 0; i < mytargets.Count(); i++)
             {
