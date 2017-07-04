0f8f29128dd492a732b2c2d470f2cf2cf0d51a5f
NitescuLucian <nitesculuci@gmail.com>
2017-07-04 14:05:09 +0300

    Cleaning

--- NliWebSec/sourcecodeleak.cs
+++ NliWebSec/sourcecodeleak.cs
@@ -16,9 +16,8 @@ namespace NliWebSec
         {
             List<string> mytargets = new List<string>();
             HttpWebResponse response = null;
-            HttpWebResponse response1 = null;
+            HttpWebResponse response2 = null;
             string html = null;
-            string html1 = null;
             Console.WriteLine("Please enter the root url of your target: ");
             string url = Console.ReadLine();
             Console.WriteLine("Please enter the level: ");
@@ -74,80 +73,32 @@ namespace NliWebSec
                     mytargets.Add(item.Value);
                 }
             }
-/*
-            if (lvl > 1)
-            {
-                for (int i = 0; i < lvl; i++)
-                {
-                    for (int j = 0; j < mytargets.Count; j++)
-                    {
-                        string url1 = mytargets[j];
-                        if (url1 != null)
-                        {
-                            // reading the html response from the target
-                            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url1);
-                            if (request1 != null)
-                            {
-                                try
-                                {
-                                    response1 = (HttpWebResponse)request1.GetResponse();
-                                }
-                                catch (System.Net.WebException ex)
-                                {
-                                    Debug.WriteLine("Exception Message: " + ex.Message);
-                                }
-                                if (response1 != null)
-                                {
-                                    if (response1.StatusCode == HttpStatusCode.OK)
-                                    {
-                                        Stream receiveStream1 = response1.GetResponseStream();
-                                        StreamReader readStream1 = null;
-
-                                        if (response1.CharacterSet == null)
-                                        {
-                                            readStream1 = new StreamReader(receiveStream1);
-                                        }
-                                        else
-                                        {
-                                            readStream1 = new StreamReader(receiveStream1, Encoding.GetEncoding(response1.CharacterSet));
-                                        }
-
-                                        html1 = readStream1.ReadToEnd();
-
-                                        response1.Close();
-                                        readStream1.Close();
-                                    }
-                                }
-                            }
-                        }
-                        //searching for urls / paths in the html response of the target
-                        if (html1 != null)
-                        {
-                            foreach (Match item1 in Regex.Matches(html1, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
-                            {
-                                string draft1 = item1.Value;
-                                //the role of this if is to filter false positives and 3rd parties resources from crawl
-                                if (draft1.StartsWith(url))
-                                {
-                                    mytargets.Add(item1.Value);
-                                }
-                            }
-                        }
-
-                    }
-
-                }
-            }
-*/
             mytargets.ToArray();
-
+            Console.WriteLine("At this point we found out: " + mytargets.Count() + " urls");
             for (int i = 0; i < mytargets.Count(); i++)
             {
-                Console.WriteLine(mytargets[i]);
+                Console.WriteLine(mytargets[i] + " ");
+                HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(mytargets[i]);
+                request1.Timeout = 400;
+                if (request1 != null)
+                {
+                    try
+                    {
+                        response2 = (HttpWebResponse)request1.GetResponse();
+                    }
+                    catch (System.Net.WebException ex)
+                    {
+                        Debug.WriteLine("Exception Message: " + ex.Message);
+                    }
+                    if (response2 != null)
+                    {
+                        Console.Write((int)response2.StatusCode);
+                    }
+                }
+                
+                Console.WriteLine();
+                Console.ReadLine();
             }
-            Console.WriteLine("At this point we found out: " + mytargets.Count() + " urls");
-            Console.WriteLine();
-            Console.ReadLine();
         }
     }
 }
