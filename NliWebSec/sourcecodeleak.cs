161a8e4173af19988e0bf6cbde7efda5e43ad25b
NitescuLucian <nitesculuci@gmail.com>
2017-07-04 10:18:35 +0300

    One level url crawling

--- NliWebSec/sourcecodeleak.cs
+++ NliWebSec/sourcecodeleak.cs
@@ -13,9 +13,18 @@ namespace NliWebSec
     {
         static void Main(string[] args)
         {
+            List<string> mytargets = new List<string>();
+
             string html = null;
             Console.WriteLine("Please enter the url of your target: ");
             string url = Console.ReadLine();
+            /*Console.WriteLine("Please enter the level: ");
+            int lvl = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < lvl; i++)
+            {
+
+            }*/
             // reading the html response from the target
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -41,8 +50,15 @@ namespace NliWebSec
             //searching for urls / paths in the html response of the target
             foreach (Match item in Regex.Matches(html, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
             {
-                Console.WriteLine(item.Value);
+                mytargets.Add(item.Value);
             }
+            mytargets.ToArray();
+            for (int i = 0; i < mytargets.Count(); i++)
+            {
+                Console.WriteLine(mytargets[i]);
+            }
+            Console.WriteLine(mytargets);
+
             Console.WriteLine();
             Console.ReadLine();
         }
